28531d7d37b78177763689f58dc06cbe1bf50309
workshopnumber00001-ui <workshopnumber00001@gmail.com>
2026-07-07 07:29:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = "8525803667:AAEjpylk_qaZ62O_gIZmch46S8VQNYxcY1M"
+    BOT_TOKEN = "8641304240:AAFGFuvucyRRitrHmgQBpxuPWIFdsZFltvI"
     DB_NAME = "nattu"
-    API_ID = "6886135"
-    API_HASH = "ee20a1c8a8e44eaa638b7254cbcc3012"
-    ADMIN_ID = 2118600611
-    DB_URL = "mongodb+srv://nattu:nattu@cluster0.quvds.mongodb.net/?appName=Cluster0"
-    LOG_CHANNEL = "-1003744110162" # Your Log Channel ID (Bot ko ADMIN BNAYE)
+    API_ID = "34422904"
+    API_HASH = "7e0002469784f47fc08a6b3d93d7ebed"
+    ADMIN_ID = "5349573682)"
+    DB_URL = "mongodb+srv://adarshppandey937:uIoPcln9vXQBF0vP@cluster0.o9mn6hb.mongodb.net/?"
+    LOG_CHANNEL = "-1003422914923" # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = "https://t.me/Natking_uploader_bot"
     TUTORIAL_VIDEO = "https://t.me/Natking_uploader_bot"
